2bbeebec8c1ba4c62600dc5233c6726ef658464d
Ngeyen Victory <ngeyennv@gmail.com>
2023-07-27 00:23:47 +0200

    delete attr

--- database/schemas.py
+++ database/schemas.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
-
-
+from datetime import datetime
 
 class FamilyCodeBase(BaseModel):
     
     id: int | None
     family_code: str
-    is_registered: bool
+    is_registered: bool | None
+    registered: datetime | None
     
-    class Config:
-        from_attributes=True
 
 
