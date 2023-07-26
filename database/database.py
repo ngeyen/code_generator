ba838461ffc0413c595af36e925458d6fd34ff79
Ngeyen Victory <ngeyennv@gmail.com>
2023-07-27 00:23:12 +0200

    fixed database config

--- database/database.py
+++ database/database.py
@@ -1,12 +1,19 @@
-from sqlmodel import Field, Session, create_engine
+from sqlmodel import Session, create_engine
 
 
+import os
+from sqlmodel import create_engine, SQLModel, Session
+
+
+# DATABASE_URL = os.environ.get("DATABASE_URL")
 SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={'check_same_thread': False}) 
 
 
+def init_db():
+    SQLModel.metadata.create_all(engine)
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-
-SessionLocal = Session(engine)
-
+def get_session():
+    with Session(engine) as session:
+        yield session
