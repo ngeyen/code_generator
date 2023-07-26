fb786ddacd4a0554b0d9cf356619ffd80acdd2f8
Ngeyen Victory <ngeyennv@gmail.com>
2023-07-27 00:23:27 +0200

    update config to work with sqlmodel

--- database/models.py
+++ database/models.py
@@ -1,19 +1,21 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from typing import Optional
 
-from .database import Base
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 
 
+class FamilyCodeBase(SQLModel):
+    family_code: str
+    
 
 
-
-class FamilyCodeModel(SQLModel, table=True):
+class FamilyCodeModel(FamilyCodeBase, table=True):
     __tablename__ = "family_codes"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    family_code = str
-    registered: Optional[DateTime] = Field(default= datetime.now())
+    registered: Optional[datetime] = Field(default= datetime.now())
     is_registered: Optional[bool] = Field(default=False)
 
+
+class FamilyCodeCreate(FamilyCodeBase):
+    pass
