20dd975bcfe4c9fcf3f7dca72987e565d76711a7
Ngeyen Victory <ngeyennv@gmail.com>
2023-07-27 00:22:43 +0200

    update family code gen

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from fastapi import status, Depends, HTTPException
-from sqlmodel import SQLModel,Session
+from sqlmodel import Session
 
 
-from api.generator import code_generator, string_generator, number_generator
+from api.generator import code_generator
 
-from database.database import  engine
-from database import crud, models, schemas
-from setup.db import get_db
+from database.database import  get_session as get_db, init_db
+from database import crud, schemas
 from setup.api import app
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
 
 
 
@@ -32,24 +26,28 @@ def read_family_codes(skip: int = 0, limit: int = 100, used:bool=False, db: Sess
     return family_codes  
 
 
-@app.get("/generate/{prefix}/{size}", tags=["Generate"], response_model=list[schemas.FamilyCodeBase], description="To generate a unique code, you can make a request to the `/generate` endpoint without providing any additional parameters. The default code length will be used (8 characters)",)
-async def codes_generator_api_view(prefix:str, size:int, db:Session=Depends(get_db)):
-    code = code_generator(prefix=prefix)
-    if code == False:
-        raise HTTPException(status_code=400, detail='Prefix longer than 8 characters')
+@app.get("/generate/{prefix}/{batch_size}", tags=["Generate"], response_model=list[schemas.FamilyCodeBase], description="To generate a unique code, you can make a request to the `/generate` endpoint without providing any additional parameters. The default code length will be used (8 characters)",)
+async def codes_generator_api_view(prefix:str, batch_size:int, db:Session=Depends(get_db)):
     codes_list = []
-    for i in range(size):
-        
+    for i in range(batch_size):
+        code = code_generator(prefix=prefix)
+        if code == False:
+            raise HTTPException(status_code=400, detail='Prefix longer than 8 characters')
+    
         db_fcode = crud.get_family_code(db, code=code)
         if not db_fcode:
             pass
         family_code = schemas.FamilyCodeBase(family_code=code, is_registered=False )
-        new_code = crud.create_family_code(db, family_code=family_code)
+        new_code = crud.create_family_code(db, serializer=family_code)
         codes_list.append(new_code)
 
     return codes_list
 
 
 
-if __name__ == "__main__":
-    create_db_and_tables()
+
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
